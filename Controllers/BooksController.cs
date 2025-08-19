02491ca438eccd9bee5643b2d0043e934b10fc62
Samin <samin.chowdhury68@qmail.cuny.edu>
2025-08-19 00:34:51 -0400

    Add project files.

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using BookApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> books = new List<Book>
+        {
+
+        };
+    }
+}
