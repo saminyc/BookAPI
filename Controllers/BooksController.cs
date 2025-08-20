404e661435a436254cd8ccf531c19efc103c12b9
Samin <samin.chowdhury68@qmail.cuny.edu>
2025-08-19 23:57:02 -0400

    routes with id set up

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -10,7 +10,28 @@ namespace BookApi.Controllers
     {
         static private List<Book> books = new List<Book>
         {
-
+            new Book { Id = 1, Title = "1984", Author = "George Orwell", YearPublished = 1949 },
+            new Book { Id = 2, Title = "To Kill a Mockingbird"},
+            new Book { Id = 3, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", YearPublished = 1925 },
+            new Book { Id = 4, Title = "Pride and Prejudice", Author = "Jane Austen", YearPublished = 1813 },
+            new Book { Id = 5, Title = "The Catcher in the Rye", Author = "J.D. Salinger", YearPublished = 1951 },
+            new Book { Id = 6, Title = "Brave New World", Author = "Aldous Huxley", YearPublished = 1932 }
         };
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Book>> GetBookById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book==null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
     }
 }
